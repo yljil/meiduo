db8d13923a3249c79c21b62306cf58ad8d398431
梁礼茏 <2025474175@qq.com>
2025-12-31 14:57:30 +0800

    QQ登陆的账号是否绑定了账号

--- apps/oauth/urls.py
+++ apps/oauth/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.oauth.views import QQLoginURLView
+from apps.oauth.views import QQLoginURLView,OauthQQView
 
 urlpatterns = [
     path('qq/authorization/', QQLoginURLView.as_view()),
+    path('oauth_callback/',OauthQQView.as_view()),
 ]
--- apps/oauth/views.py
+++ apps/oauth/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import login
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
 from QQLoginTool.QQtool import OAuthQQ
+
+from apps.oauth.models import OAuthUser
 from meiduo import settings
 from django.http import JsonResponse
 
@@ -14,3 +17,30 @@ class QQLoginURLView(View):
                      state='xxxxx')  #不知道什么意识
         qq_login_url = qq.get_qq_url()
         return JsonResponse({'code':0,'errmsg':'ok','login_url':qq_login_url})
+
+class OauthQQView(View):
+    def get(self, request):
+        code = request.GET.get('code')
+        if code is None:
+            return JsonResponse({'code':'400','errmsg':'参数不全'})
+        qq = OAuthQQ(client_id=settings.QQ_CLIENT_ID, #appid
+                     client_secret=settings.QQ_CLIENT_SECRET, #appsecret
+                     redirect_uri=settings.QQ_CALLBACK_URL, #跳转的页面
+                     state='xxxxx')  #不知道什么意识
+        # '40A140CFBD7287354A62906C454E8937'
+        token = qq.get_access_token(code)
+        openid = qq.get_open_id(token)
+
+
+        try:
+            qquser = OAuthUser.objects.get(openid=openid)
+        except OAuthUser.DoesNotExist:
+            #没有绑定过
+            response = JsonResponse({'code':'400','access_token':openid})
+            return response
+        else:
+            #绑定过
+            login(request, qquser.user)
+            response = JsonResponse({'code':0,'errmsg':'ok'})
+            response.set_cookie('username', qquser.user.username)
+            return response
